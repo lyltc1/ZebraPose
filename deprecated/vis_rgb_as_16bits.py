173094864f9646e137e0ee762feca79ebb28f8e0
lyltc1 <870767645@qq.com>
2024-12-16 19:44:35 +0800

    update

--- deprecated/vis_rgb_as_16bits.py
+++ deprecated/vis_rgb_as_16bits.py
@@ -7,7 +7,7 @@ from binary_code_helper.class_id_encoder_decoder import RGB_image_to_class_id_im
 img_path = "/home/data/vis/img.jpg"
 img = cv2.imread(img_path)
 class_id_image = RGB_image_to_class_id_image(img)
-class_code_images = class_id_image_to_class_code_images(class_id_image, iteration=8, number_of_class=256)
+class_code_images = class_id_image_to_class_code_images(class_id_image, iteration=16, number_of_class=256*256)
 
 # shape of class_code_images is (H, W, 8)
 # save each channel as a separate image
